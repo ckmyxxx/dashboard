40f7c5d859ddf23ef8d447f3b4c30e5b16076b59
ckmyxxx <ckmyxxx@gmail.com>
2021-01-08 15:07:54 +0800

    remove broken API of YAHOO WEATHER

--- js/photo.js
+++ js/photo.js
@@ -1,30 +1,33 @@
+let j = Number(localStorage.getItem('j')) || 0;
+const photo = [
+    "Amsterdam,Holland",
+    "Bluecave,Iceland",
+    "Bluelagoon,Iceland",
+    "Hverfjall,Iceland",
+    "Iceriver,Iceland",
+    "Jökulsárlón,Iceland",
+    "Luzern,Swiss",
+    "Reykjavík,Iceland",
+    "Seljalandsfoss,Iceland",
+    "Varmahlíð,Iceland",
+    "Zermatt,Swiss"
+];
+
 $(function () {
-    var j = Number(localStorage.getItem('j')) || 0;
-    var photo = [
-        "Amsterdam,Holland",
-        "Bluecave,Iceland",
-        "Bluelagoon,Iceland",
-        "Hverfjall,Iceland",
-        "Iceriver,Iceland",
-        "Jökulsárlón,Iceland",
-        "Luzern,Swiss",
-        "Reykjavík,Iceland",
-        "Seljalandsfoss,Iceland",
-        "Varmahlíð,Iceland",
-        "Zermatt,Swiss"
-    ];
+    // 綁定背景圖片
+    $("#imgDescribe").click(changeImg);
+
+    changeImg();
+});
+
+const changeImg = () => {
+
+    if (j < photo.length - 1) {
+        j++;
+    } else {
+        j = 0;
+    }
     $("#background").attr("style", "background-image: url(./img/" + photo[j] + ".JPG)");
     $("#imgDescribe").text("@ " + photo[j]);
-    // var j = 0;
-    // 綁定背景圖片
-    $("#imgDescribe").click(function () {
-        if (j < photo.length - 1) {
-            j++;
-        } else {
-            j = 0;
-        }
-        $("#background").attr("style", "background-image: url(./img/" + photo[j] + ".JPG)");
-        $("#imgDescribe").text("@ " + photo[j]);
-        localStorage.setItem("j", j);
-    });
-});
+    localStorage.setItem("j", j);
+};
